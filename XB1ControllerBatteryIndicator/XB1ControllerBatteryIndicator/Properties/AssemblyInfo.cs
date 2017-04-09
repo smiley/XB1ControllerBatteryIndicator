49523b31f27bf6bc85003169017ba05e757d7c34
NiyaShy <NiyaShy@home>
2017-04-09 14:34:17 +0200

    changed Task.Run() to a background thread. Replaced recursive method call with while loop. Moved balloon declaration to class level.

--- XB1ControllerBatteryIndicator/XB1ControllerBatteryIndicator/Properties/AssemblyInfo.cs
+++ XB1ControllerBatteryIndicator/XB1ControllerBatteryIndicator/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XB1ControllerBatteryIndicator")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
